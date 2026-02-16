154fd060e8591fbd8726d5b16d27f0ba7c74232b
Nocdamso <adsnmf@gmail.com>
2026-02-16 16:55:04 +0900

    TestCommit

---
+++ XRP104_ToyProject01_01Team/Assets/Scripts/HB_Test/PlayerHit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour, IDamagable
+{
+    private bool _isHit = false;
+    private bool _isDead;
+
+    public void TakeDamage(float damage)
+    {
+        
+    }
+}
