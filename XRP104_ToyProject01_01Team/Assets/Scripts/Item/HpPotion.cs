e99d2591f824979f023739a68a18811949f5d0de
Jade <jade16work@gmail.com>
2026-02-17 10:34:24 +0900

    [feat] Enemy item drop

---
+++ XRP104_ToyProject01_01Team/Assets/Scripts/Item/HpPotion.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HpPotion : ItemBase, IInteractable
+{
+    [SerializeField] [Range(1, 100)] private float _healValue;
+    
+    private void Awake() => Init();
+    private void OnEnable() => Drop();
+    
+    protected override void Init()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Interact(PlayerController controller)
+    {
+        controller.HealUp(_healValue);
+    }
+}
--- XRP104_ToyProject01_01Team/Assets/Scripts/Manager/DataManager.cs
+++ XRP104_ToyProject01_01Team/Assets/Scripts/Manager/DataManager.cs
@@ -1,14 +1,26 @@
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DataManager : Singleton<DataManager>
 {
     [SerializeField] private List<StageInfo> _stages = new List<StageInfo>();
+    [SerializeField] private List<ItemBase> _items = new List<ItemBase>();
     
     private void Awake() => SingletonInit();
 
     public void LoadStage(int stageNumber)
     {
         Instantiate(_stages[stageNumber]);
+    }
+
+    public void DropRandomItem(Transform owner)
+    {
+        int index = Random.Range(0, _items.Count);
+
+        Instantiate(_items[index])
+            .transform
+            .SetPositionAndRotation(owner.position, quaternion.identity);
     }
 }
