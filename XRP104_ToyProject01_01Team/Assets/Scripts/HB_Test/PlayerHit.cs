b845f4224289075b550c87c9e8c226cad057995a
Nocdamso <adsnmf@gmail.com>
2026-02-17 11:42:32 +0900

    플레이어 피격판정입니다.

--- XRP104_ToyProject01_01Team/Assets/Scripts/HB_Test/PlayerHit.cs
+++ XRP104_ToyProject01_01Team/Assets/Scripts/HB_Test/PlayerHit.cs
@@ -2,11 +2,41 @@ using UnityEngine;
 
 public class PlayerHit : MonoBehaviour, IDamagable
 {
-    private bool _isHit = false;
     private bool _isDead;
+    private float _currentPlayerHp;
+    private Animator _animator;
+    private PlayerController _playerController;
+    private LayerMask _layerMask;
+
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _playerController = GetComponent<PlayerController>();
+        _isDead = false;
+    }
 
     public void TakeDamage(float damage)
     {
+        _currentPlayerHp = _playerController._hP;
+
+        _currentPlayerHp -= damage;
+
+        if (_currentPlayerHp <= 0)
+        {
+            _isDead = true;
+            _animator.SetTrigger("Die");
+        }
         
     }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if(((1 << collision.gameObject.layer) & _layerMask.value) != 0)
+        {
+            
+            Debug.Log("충돌");
+            TakeDamage(10);
+        }
+    }*/
 }
